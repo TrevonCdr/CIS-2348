49ef3e2769786bcd0002ad0f015daf12d2170540
TrevonCdr <89557721+TrevonCdr@users.noreply.github.com>
2021-09-14 11:45:55 -0500

    Add files via upload

---
+++ Homework_1/main.py
@@ -0,0 +1,6 @@
+#Trevon McKenzie
+#1880601
+
+userNum = int(input())              #receive integer input from user
+userNumSquared = userNum * userNum  #square the value of userNum
+print(userNumSquared)               #output value of userNumSquared
