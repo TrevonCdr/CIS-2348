bb4ec615e1ed40e38f52927069fbc6d1aff65dca
TrevonCdr <89557721+TrevonCdr@users.noreply.github.com>
2021-09-14 11:47:14 -0500

    Add files via upload

---
+++ Homework_1/main.py
@@ -0,0 +1,11 @@
+# Trevon McKenzie
+# 1880601
+
+user_num1 = int(input("Enter integer: "))
+print("You entered: {}".format(user_num1))
+print(user_num1, "squared is", user_num1 * user_num1)             #output user_num1 squared
+print("And {} cubed is".format(user_num1), user_num1 ** 3, '!!')  #output user_num1 cubed
+
+user_num2 = int(input("Enter another integer: "))
+print("{} + {} is".format(user_num1, user_num2), user_num1 + user_num2)  #output sum of input integers
+print("{} * {} is".format(user_num1, user_num2), user_num1 * user_num2)  #output product of input integers
