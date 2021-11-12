e851dde6f7f9d7def6b094196f5f80caed38cb94
TrevonCdr <89557721+TrevonCdr@users.noreply.github.com>
2021-11-12 15:44:08 -0600

    Add files via upload

---
+++ Homework 3/main.py
@@ -0,0 +1,10 @@
+# Trevon McKenzie
+# 1880601
+
+words = input()            # accept words from user
+words_list = words.split() # make list of words
+
+for word in words_list:             # count the number of times the word is in the list
+    count = words_list.count(word)
+    print(word, count)              # output the word along with count
+
